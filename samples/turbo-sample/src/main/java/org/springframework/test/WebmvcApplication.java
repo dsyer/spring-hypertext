2ddd04c2515b15fe6c7db98a2d0b7ddb2aa19cdd
Dave Syer <dsyer@vmware.com>
2024-05-16 08:38:11 +0100

    Organise imports

--- samples/turbo-sample/src/main/java/org/springframework/test/WebmvcApplication.java
+++ samples/turbo-sample/src/main/java/org/springframework/test/WebmvcApplication.java
@@ -1,19 +1,10 @@
 package org.springframework.test;
 
-import java.util.LinkedHashMap;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.util.MimeType;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.context.annotation.RequestScope;
-import org.thymeleaf.spring6.SpringTemplateEngine;
-import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @SpringBootApplication
 public class WebmvcApplication {
